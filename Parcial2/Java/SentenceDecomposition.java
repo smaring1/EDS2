198d2f872820f934d84276529ea7b8b98206c430
Simón Marín Giraldo <smaring1@eafit.edu.co>
2020-04-16 23:02:01 -0500

    File name changed to the one requested

---
+++ Parcial2/Java/SentenceDecomposition.java
@@ -0,0 +1,11 @@
+public class SentenceDecomposition {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+
+    public static int decompose(String s, String [] words) {
+        int sum = 0;
+        //...
+        return sum;
+    }
+}
