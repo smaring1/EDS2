636168f9741b0354b2ec2495953addc40e7be40c
Simón Marín Giraldo <smaring1@eafit.edu.co>
2020-04-29 13:36:37 -0500

    Se agregó el método para ordenar palabras que después servirá para la comparación

--- Parcial2/Java/SentenceDecomposition.java
+++ Parcial2/Java/SentenceDecomposition.java
@@ -1,14 +1,38 @@
+import java.util.Arrays;
+
 public class SentenceDecomposition {
     public static void main(String[] args) {
         String s = "abba";
         String [] words = {"ab", "ac", "ad"};
-        System.out.println(decompose(s, words));
+        //System.out.println(decompose(s, words));
+        System.out.println(wordSort("cab"));
     }
 
     public static int decompose(String s, String [] words) {
         int sum = 0;
-        //...
-        //Write you code here
+        String [] wordsCopy = new String[words.length];
+        for (int i = 0; i < words.length; i++) {
+            wordsCopy[i] = words[i];
+        }
+
+        String current = "";
+        for (int i = 0; i < words.length; i++) {
+
+        }
+
         return sum;
+    }
+
+    public static String wordSort(String s) {
+        String [] divided = new String[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            divided[i] = s.charAt(i) + "";
+        }
+        Arrays.sort(divided);
+        String result = "";
+        for (int i = 0; i < divided.length; i++) {
+            result += divided[i];
+        }
+        return result;
     }
 }
