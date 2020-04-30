2b691be0d77fcdea81278d868ba1c570312c156f
Simón Marín Giraldo <smaring1@eafit.edu.co>
2020-04-29 21:33:17 -0500

    Se agregó la dunción cost que calcula el costo de pasar de un String a otro y se agregó documentación de los métodos.

--- Parcial2/Java/SentenceDecomposition.java
+++ Parcial2/Java/SentenceDecomposition.java
@@ -1,38 +1,70 @@
 import java.util.Arrays;
+import java.util.LinkedList;
 
+/**
+ * This class solves the Sentence
+ * Decomposition problem from
+ * topcoder which was used as
+ * Data Structures and Algorithms II
+ * second exam.
+ */
 public class SentenceDecomposition {
     public static void main(String[] args) {
         String s = "abba";
         String [] words = {"ab", "ac", "ad"};
-        //System.out.println(decompose(s, words));
-        System.out.println(wordSort("cab"));
+        decompose(s, words);
     }
 
     public static int decompose(String s, String [] words) {
+        LinkedList<String> list = new LinkedList<>();
         int sum = 0;
-        String [] wordsCopy = new String[words.length];
+        String [] wordsSorted = new String[words.length];
+        String aux = "";
         for (int i = 0; i < words.length; i++) {
-            wordsCopy[i] = words[i];
+            wordsSorted[i] = words[i];
+            wordsSorted[i] = wordSort(wordsSorted[i]);
         }
 
-        String current = "";
-        for (int i = 0; i < words.length; i++) {
 
-        }
 
         return sum;
     }
 
+    /**
+     * This method sorts the
+     * characters in a String
+     * @param s String to be sorted
+     * @return sorted String
+     */
     public static String wordSort(String s) {
-        String [] divided = new String[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            divided[i] = s.charAt(i) + "";
-        }
+        char [] divided = s.toCharArray();
         Arrays.sort(divided);
         String result = "";
         for (int i = 0; i < divided.length; i++) {
             result += divided[i];
         }
         return result;
+    }
+
+    /**
+     * This methid calculates
+     * the cost of converting
+     * first String into second
+     * String.
+     * Cost is defined as the
+     * number of characters in
+     * which two Strings differ.
+     * @param first
+     * @param second
+     * @return cost
+     */
+    public static int cost(String first, String second) {
+        int cost = 0;
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != second.charAt(i)) {
+                cost++;
+            }
+        }
+        return cost;
     }
 }
