9f8e7b801604e99131f7c89bc3ffe5bb7ec56595
Simón Marín Giraldo <smaring1@eafit.edu.co>
2020-05-02 16:02:56 -0500

    Se empezó a documentar el código línea por línea

--- Parcial2/Java/SentenceDecomposition.java
+++ Parcial2/Java/SentenceDecomposition.java
@@ -24,12 +24,12 @@ public class SentenceDecomposition {
      * @return
      */
     public static int decompose(String s, String [] words) {
-        int length = s.length();
-        int [] memorization = new int[length + 1];
-        for (int i = 0; i < memorization.length; i++) { //Filling the DP array with max value.
-            memorization[i] = Integer.MAX_VALUE;
+        int length = s.length(); // Copying the length of the original sentence into a new String variable
+        int [] memorization = new int[length + 1]; // Declaring the dynamic programming array to save the calculated values
+        for (int i = 0; i < memorization.length; i++) { // Going through the DP array
+            memorization[i] = Integer.MAX_VALUE; // Filling the DP array with max value.
         }
-        memorization[0] = 0;
+        memorization[0] = 0; // Assigning 0 to the first position in the DP array as usually done in DP solutions
 
         for (int i = 0; i < length; i++) if (memorization[i] < Integer.MAX_VALUE) {
             for (String a: words) if (i + a.length() <= length && areAnagrams(a, s.substring(i, i + a.length()))) {
@@ -41,22 +41,6 @@ public class SentenceDecomposition {
             return -1;
         }
         return memorization[length];
-
-        /*
-        int sum = 0;
-        String [] wordsSorted = new String[words.length];
-        String aux = "";
-
-        for (int i = 0; i < words.length; i++) {
-            wordsSorted[i] = words[i];
-            wordsSorted[i] = wordSort(wordsSorted[i]);
-        }
-
-
-
-        return sum;
-
-         */
     }
 
     /**
@@ -66,17 +50,17 @@ public class SentenceDecomposition {
      * @return sorted String
      */
     public static String wordSort(String s) {
-        char [] divided = s.toCharArray();
-        Arrays.sort(divided);
-        String result = "";
-        for (char c : divided) {
-            result += c;
+        char [] divided = s.toCharArray(); // Declaring a character array and filling it with the characters in String s
+        Arrays.sort(divided); // Sorting the character array
+        String result = ""; // Declaring a new String that will be returned
+        for (char c : divided) { // Going through the character array
+            result += c; // Concatenating each character in the array to the result String
         }
-        return result;
+        return result; // Returning the new String with its characters sorted
     }
 
     /**
-     * This methid calculates
+     * This method calculates
      * the cost of converting
      * first String into second
      * String.
